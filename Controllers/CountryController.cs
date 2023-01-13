a19e3e9c1d33ab262727d96e04d82e597464bae3
Clément Legay <clementlegay@MacBook-Pro-de-Clement.local>
2023-01-13 14:12:43 +0100

    end country creation

--- Controllers/CountryController.cs
+++ Controllers/CountryController.cs
@@ -1,3 +1,4 @@
+using GuacAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 using GuacAPI.Services;
 
@@ -23,16 +24,69 @@ public class CountryController : ControllerBase
     #endregion
     
     [HttpGet]
-    public async Task<IActionResult> Get()
+    public async Task<IActionResult> GetAllCountries()
     {
         var countries = await _countryService.GetAllCountries();
 
         if(countries.Count == 0)
         {
-            return NoContent();
+            NoContent();
         }
         return Ok(countries);
     }
+    
+    [HttpGet]
+    [Route("{id}")]
+    public async Task<IActionResult> GetOne(int id)
+    {
+        var country = await _countryService.GetOne(id);
 
+        if(country == null)
+        {
+            NotFound();
+        }
+        return Ok(country);
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> AddOne(Country country)
+    {
+        var newCountry = await _countryService.AddCountry(new Country()
+        {
+            Name = country.Name
+        });
 
+        return Ok(newCountry);
+    }
+    
+    [HttpPut]
+    [Route("{id}")]
+    public async Task<IActionResult> UpdateOne(int id, Country request)
+    {
+        var updatedCountry = await _countryService.UpdateCountry(id, new Country()
+        {
+            Name = request.Name
+        });
+        
+        if (updatedCountry == null)
+        {
+            NotFound();
+        }
+
+        return Ok(updatedCountry);
+    }
+    
+    [HttpDelete]
+    [Route("{id}")]
+    public async Task<IActionResult> DeleteCountry(int id)
+    {
+        var deletedCountry = await _countryService.DeleteCountry(id);
+
+        if (deletedCountry == null)
+        {
+            NotFound();
+        }
+
+        return Ok(deletedCountry);
+    }
 }
